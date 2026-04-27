5c221a5dad2fb4fcbe8c40e2d7b6d8392911cbb7
Pham Huynh Bao Ngoc <phbaongoc2506@gmail.com>
2026-04-27 15:04:10 +0700

    PhamHuynhBaoNgoc_ Tetromino

---
+++ Tetromino.java
@@ -0,0 +1,98 @@
+public class Tetromino{
+    protected PieceType type;
+    protected int [] [] shape;
+    protected int x;
+    protected int y;
+    protected int [] [] color;
+
+    private static final int [] [] [] SHAPES ={
+        //I
+        {{1,1,1,1}},
+        //O
+        {{1,1}, {1,1}}
+        //T
+        {{0,1,0}, {1,1,1}}
+        //S
+        {{0,1,1}, {1,1,0}}
+        //Z
+        {{1,1,0}, {0,1,1}}
+        //J
+        {{1,0,0},{1,1,1}}
+        //L
+        {{0,0,1}, {1,1,1}}
+};
+
+public void rotate() {
+    int rows = shape.length;
+    int cols = shape[0].length;
+    int[][] rotated = new int[cols] [rows];
+
+    for (int r = 0, r < rows; r++){
+        for (int c = 0, c< cols; c++){
+            rotated[c] [rows -1 - r] = shape[r] [c];
+        }
+    }
+    shape = rotated;
+}
+
+public void rotatedBack(){
+    int rows = shape.length;
+    int cols = shape[0].length;
+
+    for(int r = 0; r< rows; r++){
+        for(int c=0; c<cols; c++){
+            rotated[cols - 1 - c] [r] = shape [r][c];
+        }
+    }
+    shape =rotated;
+}
+
+public boolean moveDown(){
+    y++;
+    return true;
+}
+
+public void moveLeft(){
+    x--;
+}
+
+public void moveRight(){
+    x++;
+}
+
+public void moveUp(){
+    y--;
+}
+
+public void moveBackLeft(){
+    x++;
+}
+
+public void moveBackRight(){
+    x--;
+}
+
+public PieceType getType(){
+    return type;
+}
+
+public int[] [] getShape(){
+    return shape;
+}
+
+public int getX (){
+    return x;
+}
+
+public int getY (){
+    return y;
+}
+
+public void setX(int x){
+    this.x = x;
+}
+
+public void setY(int y){
+    this.y = y;
+}
+}
